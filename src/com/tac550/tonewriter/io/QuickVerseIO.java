fc482285ca3ed0cd908dea21b46aef333631a2d7
Thomas Crosby <tac550@gmail.com>
2019-03-18 17:01:16 -0400

    Implemented quick verse saving for linux

--- src/com/tac550/tonewriter/io/QuickVerseIO.java
+++ src/com/tac550/tonewriter/io/QuickVerseIO.java
@@ -19,107 +19,73 @@ public class QuickVerseIO {
 
 	public static ArrayList<String> getBuiltinVerses() throws IOException {
 		ArrayList<String> finalList = new ArrayList<>();
-		
+
 		InputStream stream = LilyPondWriter.class.getResourceAsStream(MainApp.prefs.getBoolean(MainApp.PREFS_THOU_THY_ENABLED, false) ? "quickVersesTT.txt" : "quickVersesYY.txt");
 		InputStreamReader inputReader = new InputStreamReader(stream);
 		BufferedReader bufferedReader = new BufferedReader(inputReader);
-		
+
 		String line;
-		
+
 		while ( (line = bufferedReader.readLine()) != null) {
 			if (!line.isEmpty() && !line.startsWith("#")) {
-				finalList.add(line);	
+				finalList.add(line);
 			}
 		}
-		
+
 		bufferedReader.close();
 		inputReader.close();
 		stream.close();
-		
+
 		return finalList;
 	}
-	
+
 	public static ArrayList<String> getCustomVerses() throws IOException {
 		ArrayList<String> finalList = new ArrayList<>();
-		
-		File verseFile = new File("");
-		
-		String osName = System.getProperty("os.name").toLowerCase();
-		String fileNameString = (MainApp.prefs.getBoolean(MainApp.PREFS_THOU_THY_ENABLED, false) ? "CustomVersesTT.txt" : "CustomVersesYY.txt");
-		// Set the verse file path, which depends on the platform.
-		if (osName.startsWith("win")) {
-			verseFile = new File(System.getenv("APPDATA") + File.separator + MainApp.APPNAME + File.separator + fileNameString);
-		} if (osName.startsWith("mac")) {
-			verseFile = new File(System.getProperty("user.home") + "/Library/Preferences/" + MainApp.APPNAME + File.separator + fileNameString);
-			System.out.println(verseFile.getAbsolutePath());
-		} if (osName.startsWith("lin")) {
-			// TODO: UNKNOWN
-		}
-		
+
+		File verseFile = getPlatformSpecificVerseFile();
+
 		// If verse file doesn't exist, just return the empty final list.
 		if (!verseFile.exists()) {
 			return finalList;
 		}
-		
+
 		FileReader fileReader = new FileReader(verseFile);
 		BufferedReader bufferedReader = new BufferedReader(fileReader);
-		
+
 		String line;
-		
+
 		while ((line = bufferedReader.readLine()) != null) {
 			if (!line.isEmpty() && !line.startsWith("#")) {
-				finalList.add(line);	
+				finalList.add(line);
 			}
 		}
-		
+
 		bufferedReader.close();
 		fileReader.close();
-		
+
 		return finalList;
 	}
-	
+
 	public static void addCustomVerse(String verse) throws IOException {
-		File verseFile = new File("");
-		
-		String osName = System.getProperty("os.name").toLowerCase();
-		String fileNameString = (MainApp.prefs.getBoolean(MainApp.PREFS_THOU_THY_ENABLED, false) ? "CustomVersesTT.txt" : "CustomVersesYY.txt");
-		// Set the verse file path, which depends on the platform.
-		if (osName.startsWith("win")) {
-			verseFile = new File(System.getenv("APPDATA") + File.separator + MainApp.APPNAME + File.separator + fileNameString);
-		} if (osName.startsWith("mac")) {
-			verseFile = new File(System.getProperty("user.home") + "/Library/Preferences/" + MainApp.APPNAME + File.separator + fileNameString);
-		} if (osName.startsWith("lin")) {
-			// TODO: UNKNOWN
-		}
-		
+		File verseFile = getPlatformSpecificVerseFile();
+
 		// Create custom verse file if it doesn't already exist.
 		if (!verseFile.exists()) {
 			verseFile.getParentFile().mkdirs();
 			verseFile.createNewFile();
 		}
-		
+
 		// Write the new custom verse to the file.
 		Files.write(verseFile.toPath(), Arrays.asList(verse), StandardOpenOption.APPEND);
 	}
-	
+
 	public static boolean removeCustomVerse(String verse) throws IOException {
-		File verseFile = new File("");
-		
-		String osName = System.getProperty("os.name").toLowerCase();
-		String fileNameString = (MainApp.prefs.getBoolean(MainApp.PREFS_THOU_THY_ENABLED, false) ? "CustomVersesTT.txt" : "CustomVersesYY.txt");
-		// Set the verse file path, which depends on the platform.
-		if (osName.startsWith("win")) {
-			verseFile = new File(System.getenv("APPDATA") + File.separator + MainApp.APPNAME + File.separator + fileNameString);
-		} if (osName.startsWith("mac")) {
-			verseFile = new File(System.getProperty("user.home") + "/Library/Preferences/" + MainApp.APPNAME + File.separator + fileNameString);
-		} if (osName.startsWith("lin")) {
-			// TODO: UNKNOWN
-		}
-		
+		File verseFile = getPlatformSpecificVerseFile();
+
 		if (!verseFile.exists()) {
 			return false;
 		}
-		
+
 		File tempFile = new File(verseFile.getParent() + File.separator + "TEMP");
 
 		BufferedReader reader = new BufferedReader(new FileReader(verseFile));
@@ -135,15 +101,29 @@ public class QuickVerseIO {
 		    }
 		    writer.write(currentLine + System.getProperty("line.separator"));
 		}
-		
-		writer.close(); 
+
+		writer.close();
 		reader.close();
-		
+
 		verseFile.delete();
 		tempFile.renameTo(verseFile);
-		
+
 		// Only return true if a line was removed (otherise it must have been a built-in verse)
 		return removed;
 	}
-	
+
+	private static File getPlatformSpecificVerseFile() {
+		String osName = System.getProperty("os.name").toLowerCase();
+		String fileNameString = File.separator + MainApp.APPNAME + File.separator +
+				(MainApp.prefs.getBoolean(MainApp.PREFS_THOU_THY_ENABLED, false) ? "CustomVersesTT.txt" : "CustomVersesYY.txt");
+
+		if (osName.startsWith("win")) {
+			return new File(System.getenv("APPDATA") + fileNameString);
+		} if (osName.startsWith("mac")) {
+			return new File(System.getProperty("user.home") + "/Library/Preferences" + fileNameString);
+		} if (osName.startsWith("lin")) {
+			return new File(System.getProperty("user.home") + "/.config" + fileNameString);
+		} else return null;
+	}
+
 }
