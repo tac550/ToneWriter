194c44086c10c8fa2e91d7f8772178671ee684b6
tac550 <tac550@gmail.com>
2019-04-25 12:59:14 -0400

    Cleaned up PDF combine view controller

--- src/com/tac550/tonewriter/view/PDFCombineViewController.java
+++ src/com/tac550/tonewriter/view/PDFCombineViewController.java
@@ -1,42 +1,32 @@
 package com.tac550.tonewriter.view;
 
-import java.awt.Desktop;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
-import org.apache.pdfbox.io.MemoryUsageSetting;
-import org.apache.pdfbox.multipdf.PDFMergerUtility;
-
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.Alert;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.OverrunStyle;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.apache.pdfbox.io.MemoryUsageSetting;
+import org.apache.pdfbox.multipdf.PDFMergerUtility;
+
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class PDFCombineViewController {
 
@@ -47,7 +37,7 @@ public class PDFCombineViewController {
 	@FXML private TextField fileNameBox;
 	
 	private static final String TAB_DRAG_KEY = "pdfPane";
-	private ObjectProperty<GridPane> draggingPane = new SimpleObjectProperty<GridPane>();
+	private ObjectProperty<GridPane> draggingPane = new SimpleObjectProperty<>();
 	
 	@FXML private void initialize() {
 		dragInstructions.setVisible(false);
@@ -61,13 +51,15 @@ public class PDFCombineViewController {
 		File pdfFolder = dirChooser.showDialog(fileBox.getScene().getWindow());
 		
 		if (pdfFolder != null) {
-			List<File> allFiles = Arrays.asList(pdfFolder.listFiles());
-			for (File file : allFiles) {
-				if (file.getName().endsWith(".pdf")) {
-					addItem(file);
+			File[] allFiles = pdfFolder.listFiles();
+			if (allFiles != null) {
+				for (File file : allFiles) {
+					if (file.getName().endsWith(".pdf")) {
+						addItem(file);
+					}
 				}
 			}
-			
+
 			// Update default directory.
 			defaultDirectory = pdfFolder;
 		}
@@ -173,59 +165,49 @@ public class PDFCombineViewController {
 		removeButton.setStyle("-fx-text-fill: #ff0000; -fx-font-weight: bold");
 		
 		// Remove button behavior
-		removeButton.setOnAction((ae) -> {
-			fileBox.getChildren().remove(itemPane);
-		});
+		removeButton.setOnAction((ae) -> fileBox.getChildren().remove(itemPane));
 		
 		// Dragging behavior
-		itemPane.setOnDragOver(new EventHandler<DragEvent>() {
-            @Override
-            public void handle(DragEvent event) {
-                final Dragboard dragboard = event.getDragboard();
-                if (dragboard.hasString()
-                        && TAB_DRAG_KEY.equals(dragboard.getString())
-                        && draggingPane.get() != null) {
-                    event.acceptTransferModes(TransferMode.MOVE);
-                    event.consume();
-                }
-            }
-        });
-		itemPane.setOnDragDropped(new EventHandler<DragEvent>() {
-            public void handle(final DragEvent event) {
-                Dragboard db = event.getDragboard();
-                boolean success = false;
-                if (db.hasString()) {
-                    VBox parent = (VBox) itemPane.getParent();
-                    Object source = event.getGestureSource();
-                    int sourceIndex = parent.getChildren().indexOf(source);
-                    int targetIndex = parent.getChildren().indexOf(itemPane);
-                    List<Node> nodes = new ArrayList<Node>(parent.getChildren());
-                    if (sourceIndex < targetIndex) {
-                        Collections.rotate(
-                                nodes.subList(sourceIndex, targetIndex + 1), -1);
-                    } else {
-                        Collections.rotate(
-                                nodes.subList(targetIndex, sourceIndex + 1), 1);
-                    }
-                    parent.getChildren().clear();
-                    parent.getChildren().addAll(nodes);
-                    success = true;
-                }
-                event.setDropCompleted(success);
-                event.consume();
-            }
-        });
-		itemPane.setOnDragDetected(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                Dragboard dragboard = itemPane.startDragAndDrop(TransferMode.MOVE);
-                ClipboardContent clipboardContent = new ClipboardContent();
-                clipboardContent.putString(TAB_DRAG_KEY);
-                dragboard.setContent(clipboardContent);
-                draggingPane.set(itemPane);
-                event.consume();
-            }
-        });
+		itemPane.setOnDragOver(event -> {
+		    final Dragboard dragboard = event.getDragboard();
+		    if (dragboard.hasString()
+		            && TAB_DRAG_KEY.equals(dragboard.getString())
+		            && draggingPane.get() != null) {
+		        event.acceptTransferModes(TransferMode.MOVE);
+		        event.consume();
+		    }
+		});
+		itemPane.setOnDragDropped(event -> {
+		    Dragboard db = event.getDragboard();
+		    boolean success = false;
+		    if (db.hasString()) {
+		        VBox parent = (VBox) itemPane.getParent();
+		        Node source = (Node) event.getGestureSource();
+		        int sourceIndex = parent.getChildren().indexOf(source);
+		        int targetIndex = parent.getChildren().indexOf(itemPane);
+		        List<Node> nodes = new ArrayList<>(parent.getChildren());
+		        if (sourceIndex < targetIndex) {
+		            Collections.rotate(
+		                    nodes.subList(sourceIndex, targetIndex + 1), -1);
+		        } else {
+		            Collections.rotate(
+		                    nodes.subList(targetIndex, sourceIndex + 1), 1);
+		        }
+		        parent.getChildren().clear();
+		        parent.getChildren().addAll(nodes);
+		        success = true;
+		    }
+		    event.setDropCompleted(success);
+		    event.consume();
+		});
+		itemPane.setOnDragDetected(event -> {
+		    Dragboard dragboard = itemPane.startDragAndDrop(TransferMode.MOVE);
+		    ClipboardContent clipboardContent = new ClipboardContent();
+		    clipboardContent.putString(TAB_DRAG_KEY);
+		    dragboard.setContent(clipboardContent);
+		    draggingPane.set(itemPane);
+		    event.consume();
+		});
 		
 		ColumnConstraints column1 = new ColumnConstraints(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
 		column1.setHgrow(Priority.ALWAYS);
@@ -241,7 +223,7 @@ public class PDFCombineViewController {
 		stage.close();
 	}
 	
-	public void setDefaultDirectory(File directory) {
+	void setDefaultDirectory(File directory) {
 		defaultDirectory = directory;
 	}
 	
