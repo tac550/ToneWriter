350d3aced232b3e20727d6faa324ad9885453a5e
TOM-PC\Thomas <tac550@gmail.com>
2019-07-30 19:16:52 -0400

    Temp files are now deleted more often when possible.

--- src/com/tac550/tonewriter/view/MainSceneController.java
+++ src/com/tac550/tonewriter/view/MainSceneController.java
@@ -751,6 +751,17 @@ public class MainSceneController {
 	    }
 	}
 	private void refreshAllChords() {
+
+		File tempDir = new File(System.getProperty("java.io.tmpdir"));
+		File[] files = tempDir.listFiles();
+		for (File file : Objects.requireNonNull(files)) {
+			if (file.getName().startsWith(MainApp.APP_NAME)) {
+				if (!file.delete()) {
+					System.out.println("Failed to delete temp file " + file.getName());
+				}
+			}
+		}
+
 		for (ChantLineViewController chantLineController : chantLineControllers) {
 			chantLineController.refreshAllChords();
 	    }
