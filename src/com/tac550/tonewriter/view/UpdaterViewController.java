66a0e1a4c820eb3e2e04946dcd4c44b05372b733
tac550 <tac550@gmail.com>
2020-02-14 12:16:42 -0500

    Updater WebView now opens links in default browser

--- src/com/tac550/tonewriter/view/UpdaterViewController.java
+++ src/com/tac550/tonewriter/view/UpdaterViewController.java
@@ -1,6 +1,7 @@
 package com.tac550.tonewriter.view;
 
 import com.tac550.tonewriter.io.AutoUpdater;
+import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -8,7 +9,15 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.text.Text;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.events.EventTarget;
+import org.w3c.dom.html.HTMLAnchorElement;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.List;
 
 public class UpdaterViewController {
@@ -30,6 +39,36 @@ public class UpdaterViewController {
 		updateOnStartupBox.setSelected(MainApp.prefs.getBoolean(MainApp.PREFS_CHECK_UPDATE_APPSTARTUP, true));
 		updateOnStartupBox.selectedProperty().addListener((ov, oldVal, newVal) ->
 				MainApp.prefs.putBoolean(MainApp.PREFS_CHECK_UPDATE_APPSTARTUP, newVal));
+
+		// Open any webview links in the system's default Web browser.
+		webView.getEngine().getLoadWorker().stateProperty().addListener((ov, oldVal, newVal) -> {
+			if (newVal == Worker.State.SUCCEEDED) {
+
+				NodeList nodeList = webView.getEngine().getDocument().getElementsByTagName("a");
+				for (int i = 0; i < nodeList.getLength(); i++) {
+					Node node = nodeList.item(i);
+					EventTarget eventTarget = (EventTarget) node;
+					eventTarget.addEventListener("click", event -> {
+						EventTarget target = event.getCurrentTarget();
+						HTMLAnchorElement anchorElement = (HTMLAnchorElement) target;
+						String href = anchorElement.getHref();
+
+						try {
+							if (MainApp.OS_NAME.startsWith("lin")) {
+								Runtime.getRuntime().exec("xdg-open " + href);
+							} else {
+								Desktop.getDesktop().browse(new URL(href).toURI());
+							}
+						} catch (IOException | URISyntaxException e) {
+							e.printStackTrace();
+						}
+
+						event.preventDefault();
+					}, false);
+				}
+
+			}
+		});
 	}
 
 	@FXML private void handleUpdate() {
