02986e0b3b1ce2f1aaf94d84cbbf508b8905f0d4
Thomas Crosby <tac550@gmail.com>
2020-05-21 00:32:54 -0400

    Increased maximum heap size to 4096 megabytes on all platforms

--- build/res/BUILD_MAC.sh
+++ build/res/BUILD_MAC.sh
@@ -6,7 +6,7 @@ rm -rf java-runtime & \
 jlink --no-header-files --no-man-pages --compress=2 --strip-debug --module-path javafx-jmods* \
 --add-modules java.xml,java.scripting,java.desktop,java.management,jdk.unsupported,javafx.controls,javafx.fxml,javafx.web,\
 java.naming,jdk.charsets,jdk.crypto.ec,java.sql --output java-runtime/Contents/Home/jre && \
-jar2app ToneWriter.jar -n "ToneWriter" -d "ToneWriter" -i TWIcon.icns -b com.tac550.tonewriter \
+jar2app ToneWriter.jar -n "ToneWriter" -d "ToneWriter" -i TWIcon.icns -b com.tac550.tonewriter -j "-Xms256m -Xmx4096m" \
 -v 0.6 -s 0.6 ../mac/ToneWriter.app -r java-runtime && \
 cp -a '../../Built-in Tones' '../mac/ToneWriter.app/Contents/Built-in Tones' && \
 cp -a '../../licenses' '../mac/ToneWriter.app/Contents/licenses' && \
