17afd53d3b4cb01b88e8e1d239d15831a7193e5f
Thomas Crosby <tac550@gmail.com>
2020-05-23 16:04:29 -0400

    Updated to Java 14, modified Mac bundling process

--- build/res/BUILD_MAC.sh
+++ build/res/BUILD_MAC.sh
@@ -5,9 +5,9 @@ rm -rf ../mac/ToneWriter*.app.zip & \
 rm -rf java-runtime & \
 jlink --no-header-files --no-man-pages --compress=2 --strip-debug --module-path javafx-jmods* \
 --add-modules java.xml,java.scripting,java.desktop,java.management,jdk.unsupported,javafx.controls,javafx.fxml,javafx.web,\
-java.naming,jdk.charsets,jdk.crypto.ec,java.sql --output java-runtime/Contents/Home/jre && \
-jar2app ToneWriter.jar -n "ToneWriter" -d "ToneWriter" -i TWIcon.icns -b com.tac550.tonewriter -j "-Xms256m -Xmx4096m" \
--v 0.6 -s 0.6 ../mac/ToneWriter.app -r java-runtime && \
+java.naming,jdk.charsets,jdk.crypto.ec,java.sql --output java-runtime/ && \
+jpackage --name ToneWriter --input jar/ --main-jar tonewriter.jar --java-options "-Xms256m -Xmx4096m -Duser.dir=\$ROOTDIR" \
+--icon TWIcon.icns --runtime-image java-runtime/ --type app-image --app-version 0.6 --dest ../mac/ && \
 cp -a '../../Built-in Tones' '../mac/ToneWriter.app/Contents/Built-in Tones' && \
 cp -a '../../licenses' '../mac/ToneWriter.app/Contents/licenses' && \
 cp -a '../../lilypond' '../mac/ToneWriter.app/Contents/lilypond' && \
