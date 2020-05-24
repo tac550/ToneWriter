66d54d524c6c5b9bb63972cd1786da89db19a45c
Thomas Crosby <tac550@gmail.com>
2020-05-23 20:22:18 -0400

    Ditto

--- build/res/BUILD_WIN.cmd
+++ build/res/BUILD_WIN.cmd
@@ -6,7 +6,7 @@ jlink --no-header-files --no-man-pages --compress=2 --strip-debug --module-path 
 --add-modules java.xml,java.scripting,java.desktop,java.management,jdk.unsupported,javafx.controls,javafx.fxml,javafx.web,^
 java.naming,jdk.charsets,jdk.crypto.ec,java.sql --output java-runtime && ^
 for /f "delims=" %%x in (path.txt) do set jar_path=%%x
-java -jar "%jar_path%" TWBuild_WIN.xml && ^
+java -jar "%jar_path%" TWBuild_WIN.xml & ^
 del path.txt & ^
 rd /s /q "..\win\Built-in Tones" & ^
 rd /s /q "..\win\licenses" & ^
