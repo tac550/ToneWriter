e339f8c213f9e46450cab99f7538ed0a22545219
Thomas Crosby <tac550@gmail.com>
2024-09-13 15:51:02 -0400

    Reindent Java files

--- src/main/java/com/tac550/tonewriter/util/ToneChordRenderAdapter.java
+++ src/main/java/com/tac550/tonewriter/util/ToneChordRenderAdapter.java
@@ -18,113 +18,113 @@ import java.util.logging.Logger;
 
 public class ToneChordRenderAdapter {
 
-    private static final Logger logger = MainApp.debugChordThreads ?
-            Logger.getLogger(ToneChordRenderAdapter.class.getCanonicalName()) : null;
+	private static final Logger logger = MainApp.debugChordThreads ?
+			Logger.getLogger(ToneChordRenderAdapter.class.getCanonicalName()) : null;
 
-    public static class DoneSignal {
-        public final Lock lock;
-        public final Condition cond;
+	public static class DoneSignal {
+		public final Lock lock;
+		public final Condition cond;
 
-        public DoneSignal() {
-            lock = new ReentrantLock();
-            cond = lock.newCondition();
-        }
-    }
+		public DoneSignal() {
+			lock = new ReentrantLock();
+			cond = lock.newCondition();
+		}
+	}
 
-    private static final Map<String, File[]> uniqueChordRenders = new HashMap<>();
-    private static final Lock renderMapLock = new ReentrantLock();
-    private static final Map<String, DoneSignal> uniqueChordSig = new HashMap<>();
-    private static final Lock sigMapLock = new ReentrantLock();
+	private static final Map<String, File[]> uniqueChordRenders = new HashMap<>();
+	private static final Lock renderMapLock = new ReentrantLock();
+	private static final Map<String, DoneSignal> uniqueChordSig = new HashMap<>();
+	private static final Lock sigMapLock = new ReentrantLock();
 
-    public static String generateChordId(Chord chord, String key_sig) {
-        return chord.getFields().replace("<", "(").replace(">", ")") + "-"
-                + TWUtils.convertAccidentalSymbols(key_sig);
-    }
+	public static String generateChordId(Chord chord, String key_sig) {
+		return chord.getFields().replace("<", "(").replace(">", ")") + "-"
+				+ TWUtils.convertAccidentalSymbols(key_sig);
+	}
 
-    public static void renderChord(Chord chord, String key_sig, Consumer<File[]> exit_actions) {
-        String uuid = logger != null ? UUID.randomUUID().toString() : null;
-        Renderer renderer = new Renderer(chord, key_sig, uuid);
-        Thread thread = new Thread(renderer);
-        thread.start();
+	public static void renderChord(Chord chord, String key_sig, Consumer<File[]> exit_actions) {
+		String uuid = logger != null ? UUID.randomUUID().toString() : null;
+		Renderer renderer = new Renderer(chord, key_sig, uuid);
+		Thread thread = new Thread(renderer);
+		thread.start();
 
-        new Thread(() -> {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            if (logger != null) logger.info(String.format(Locale.US, "%s: %s: C: Finished.", uuid, generateChordId(chord, key_sig)));
-            exit_actions.accept(renderer.getResults());
-        }).start();
-    }
+		new Thread(() -> {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+			if (logger != null) logger.info(String.format(Locale.US, "%s: %s: C: Finished.", uuid, generateChordId(chord, key_sig)));
+			exit_actions.accept(renderer.getResults());
+		}).start();
+	}
 
-    public static class Renderer implements Runnable {
-        private File[] results;
+	public static class Renderer implements Runnable {
+		private File[] results;
 
-        private final Chord chord;
-        private final String key_sig;
+		private final Chord chord;
+		private final String key_sig;
 
-        private final String uuid;
+		private final String uuid;
 
-        public Renderer(Chord chord, String key_sig, String uuid) {
-            this.chord = chord;
-            this.key_sig = key_sig;
+		public Renderer(Chord chord, String key_sig, String uuid) {
+			this.chord = chord;
+			this.key_sig = key_sig;
 
-            this.uuid = uuid;
-        }
+			this.uuid = uuid;
+		}
 
-        @Override
-        public void run() {
-            final String chordID = generateChordId(chord, key_sig);
+		@Override
+		public void run() {
+			final String chordID = generateChordId(chord, key_sig);
 
-            renderMapLock.lock();
-            if (!uniqueChordRenders.containsKey(chordID)) {
-                if (logger != null) logger.info(String.format(Locale.US, "%s: %s: A: Rendering new chord.", uuid, chordID));
-                uniqueChordRenders.put(chordID, null);
-                sigMapLock.lock();
-                uniqueChordSig.put(chordID, new DoneSignal());
-                sigMapLock.unlock();
-                renderMapLock.unlock();
-                try {
-                    LilyPondInterface.renderChord(chord, key_sig, (files -> {
-                        renderMapLock.lock();
-                        uniqueChordRenders.put(chordID, files);
-                        renderMapLock.unlock();
-                        DoneSignal sig = uniqueChordSig.get(chordID);
-                        sig.lock.lock();
-                        sig.cond.signalAll();
-                        sig.lock.unlock();
-                    }));
-                    uniqueChordSig.get(chordID).lock.lock();
-                    uniqueChordSig.get(chordID).cond.await();
-                } catch (IOException | InterruptedException e) {
-                    throw new RuntimeException(e);
-                } finally {
-                    uniqueChordSig.get(chordID).lock.unlock();
-                }
-            } else if (uniqueChordRenders.get(chordID) == null) {
-                if (logger != null) logger.info(String.format(Locale.US, "%s: %s: B: Awaiting ongoing render.", uuid, chordID));
-                renderMapLock.unlock();
-                uniqueChordSig.get(chordID).lock.lock();
-                try {
-                    uniqueChordSig.get(chordID).cond.await();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                } finally {
-                    uniqueChordSig.get(chordID).lock.unlock();
-                }
-            } else {
-                renderMapLock.unlock();
-            }
+			renderMapLock.lock();
+			if (!uniqueChordRenders.containsKey(chordID)) {
+				if (logger != null) logger.info(String.format(Locale.US, "%s: %s: A: Rendering new chord.", uuid, chordID));
+				uniqueChordRenders.put(chordID, null);
+				sigMapLock.lock();
+				uniqueChordSig.put(chordID, new DoneSignal());
+				sigMapLock.unlock();
+				renderMapLock.unlock();
+				try {
+					LilyPondInterface.renderChord(chord, key_sig, (files -> {
+						renderMapLock.lock();
+						uniqueChordRenders.put(chordID, files);
+						renderMapLock.unlock();
+						DoneSignal sig = uniqueChordSig.get(chordID);
+						sig.lock.lock();
+						sig.cond.signalAll();
+						sig.lock.unlock();
+					}));
+					uniqueChordSig.get(chordID).lock.lock();
+					uniqueChordSig.get(chordID).cond.await();
+				} catch (IOException | InterruptedException e) {
+					throw new RuntimeException(e);
+				} finally {
+					uniqueChordSig.get(chordID).lock.unlock();
+				}
+			} else if (uniqueChordRenders.get(chordID) == null) {
+				if (logger != null) logger.info(String.format(Locale.US, "%s: %s: B: Awaiting ongoing render.", uuid, chordID));
+				renderMapLock.unlock();
+				uniqueChordSig.get(chordID).lock.lock();
+				try {
+					uniqueChordSig.get(chordID).cond.await();
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				} finally {
+					uniqueChordSig.get(chordID).lock.unlock();
+				}
+			} else {
+				renderMapLock.unlock();
+			}
 
-            renderMapLock.lock();
-            results = uniqueChordRenders.get(chordID);
-            renderMapLock.unlock();
-        }
+			renderMapLock.lock();
+			results = uniqueChordRenders.get(chordID);
+			renderMapLock.unlock();
+		}
 
-        public File[] getResults() {
-            return results;
-        }
+		public File[] getResults() {
+			return results;
+		}
 
-    }
+	}
 }
--- src/main/java/com/tac550/tonewriter/view/DebugStarter.java
+++ src/main/java/com/tac550/tonewriter/view/DebugStarter.java
@@ -1,7 +1,7 @@
 package com.tac550.tonewriter.view;
 
 public class DebugStarter {
-    public static void main(String[] args) {
-        MainApp.main(args);
-    }
+	public static void main(String[] args) {
+		MainApp.main(args);
+	}
 }
